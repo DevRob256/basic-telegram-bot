5fef13b8c91e8039c4cc3b869abbb44998ab6f2c
Robson Kinoshita <robsonyukiu@gmail.com>
2020-05-31 05:05:17 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intro
+A very basic Telegram bot, created just as an example for a youtube tutorial.
+
+##Copyright
+
+Use it the way you want!
+
---
+++ bot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Telegram.Bot;
+
+
+namespace bot
+{
+    class Program
+    {
+
+        static TelegramBotClient botClient = new TelegramBotClient("1268622258:AAHzyjxcLNC0ZGDayQvi0H2blDfFQXXA3Jo");
+        static void Main(string[] args)
+        {
+
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+            Console.ReadLine();
+
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+
+            Console.WriteLine(e.Message.Text);
+
+
+            if (e.Message.Text.ToUpper() == "HI") 
+            {
+                botClient.SendTextMessageAsync(e.Message.Chat.Id, "Whats up?");
+            
+            }
+
+        }
+    }
+}
